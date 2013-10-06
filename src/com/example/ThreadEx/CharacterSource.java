4b5ee051d251ee26e2fc9d7fafb17dbf117d1d82
moonlightcheese <moonlightcheese@gmail.com>
2013-10-06 13:32:53 -0400

    onResume example

--- src/com/example/ThreadEx/CharacterSource.java
+++ src/com/example/ThreadEx/CharacterSource.java
@@ -7,7 +7,7 @@ package com.example.ThreadEx;
  * Time: 5:20 PM
  * To change this template use File | Settings | File Templates.
  */
-public interface CharacterSource  {
+public interface CharacterSource {
     public void addCharacterListener(CharacterListener cl);
     public void removeCharacterListener(CharacterListener cl);
     public void nextCharacter();
--- src/com/example/ThreadEx/MainActivity.java
+++ src/com/example/ThreadEx/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ThreadEx;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +12,16 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onResume() {
+        super.onResume();
+        EditText textBox = (EditText)findViewById(R.id.characters);
+        textBox.setText("Aaaaand... we're back");
+    }
+
+    public synchronized void showCharacter(CharacterEvent ce) {
+        EditText textBox = (EditText)findViewById(R.id.characters);
+        //String textBox.getText()
     }
 }

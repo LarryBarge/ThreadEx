cbf080fda330c9ea0b4a48194f6387213f7cb7d9
Jarrod Martin <moonlightcheese@gmail.com>
2013-10-06 21:51:04 -0400

    bullshit lol

--- src/com/example/ThreadEx/MainActivity.java
+++ src/com/example/ThreadEx/MainActivity.java
@@ -7,11 +7,9 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.List;
